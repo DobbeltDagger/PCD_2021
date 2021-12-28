782268e57ff653b3811835081a7c38b7116fa021
cirkavisti <andersvisti@gmail.com>
2021-12-28 12:33:51 +0100

    Added a gallery to 2021

--- assets/2021_GalleryThumbs/buildAll.sh
+++ assets/2021_GalleryThumbs/buildAll.sh
@@ -14,16 +14,19 @@ do
 
   # newline in string and then in file:
   # https://unix.stackexchange.com/questions/191694/how-to-put-a-newline-special-character-into-a-file-using-the-echo-command-and-re
-  title="title: 'Processing Community Day @Aarhus 2019'"
-  desc="description: 'Processing Community Day @Aarhus 2019'"
-  imgUrl="imageUrl: '/assets/2020_GalleryThumbs/large/${filename}.jpg'"
-  thumbUrl="thumbUrl: '/assets/2020_GalleryThumbs/thumbs/${filename}_thumb.png'"
+  title="title: 'Processing Community Day @Aarhus 2021'"
+  desc="description: 'Processing Community Day @Aarhus 2021'"
+  imgUrl="imageUrl: '/assets/2021_GalleryThumbs/large/${filename}.jpg'"
+  thumbUrl="thumbUrl: '/assets/2021_GalleryThumbs/thumbs/${filename}_thumb.png'"
+  dateString="date: 2021-01-01"
+  excludeString='eleventyExcludeFromCollections: false'
 
   # show it here
   echo $filename
+  echo $file
   # echo $link >> html.txt
   # echo -e $link >> code/$filename.md
-  echo $'---\n'${title}$'\n'${desc}$'\n'${imgUrl}$'\n'${thumbUrl}$'\n---' > code/$filename.md
+  echo $'---\n'${title}$'\n'${desc}$'\n'${imgUrl}$'\n'${thumbUrl}$'\n'${dateString}'\n'${excludeString}'\n---' > code/$filename.md
 
   # next line checks the mime-type of the file
   IMAGE_TYPE=`file --mime-type -b "$file" | awk -F'/' '{print $1}'`
@@ -39,11 +42,13 @@ do
     pngExtension="png"
 
     # making large version
-    convert "$file" -resize 1280x854 -quality 70 "${LARGE_FOLDER}/${filename}.${jpgExtension}"
+    # convert "$file" -resize 1280x854 -quality 70 "${LARGE_FOLDER}/${filename}.${jpgExtension}"
+    # imagemagick 7 needs magick in front of convert!!! - https://stackoverflow.com/questions/3060205/error-invalid-parameter-fom-imagemagick-convert-on-windows/54106484
+    magick convert "$file" -resize 1280x854 -quality 70 "${LARGE_FOLDER}/${filename}.${jpgExtension}"
     
     # making thumb
     # convert "$file" -sample 300x300 -background none -gravity center -extent 300x300 "${THUMBS_FOLDER}/${filename}_thumb.${pngExtension}"
-    convert "$file" -sample 500x500 -background none -gravity center -extent 500x500 "${THUMBS_FOLDER}/${filename}_thumb.${pngExtension}"
+    magick convert "$file" -sample 500x325 -background none -gravity center -extent 500x325 "${THUMBS_FOLDER}/${filename}_thumb.${pngExtension}"
 
   fi     
 done
